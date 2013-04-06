de13512b0a9107b4e58dff650e41db3623b17b6f
Bastien Dejean <nihilhill@gmail.com>
2013-04-06 11:09:54 +0200

    Handle alternate font via `-F` and `-S`
    New command-line arguments are introduced to specify an alternate font.
    The alternate font parts can be specified in the strings by surrounding
    the relevant substrings with '\(' and '\)'.
    
    E.g.:
        textwidth -f sans-serif -s 10 -F serif -S 13 'foo\(bar\)baz'
    In the above example, the returned width will be:
        tw('sans-serif', 10, 'foobaz') + tw('serif', 13, 'bar')

--- textwidth.c
+++ textwidth.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <string.h>
 #include <cairo/cairo.h>
@@ -21,12 +22,14 @@ int text_width(char *font_family, int font_size, char *s) {
 int main(int argc, char *argv[]) {
     char font_family[MAXLEN] = FONT_FAMILY;
     int font_size = FONT_SIZE;
+    char *alt_font_family = NULL;
+    int alt_font_size = FONT_SIZE;
     char opt;
 
-    while ((opt = getopt(argc, argv, "hvf:s:")) != -1) {
+    while ((opt = getopt(argc, argv, "hvf:s:F:S:")) != -1) {
         switch (opt) {
             case 'h':
-                printf("textwidth [-h|-v|-f FONT|-s SIZE] STRING ...\n");
+                printf("textwidth [-h|-v|-f FONT|-s SIZE|-F ALT_FONT|-S ALT_SIZE] STRING ...\n");
                 return EXIT_SUCCESS;
                 break;
             case 'v':
@@ -40,14 +43,59 @@ int main(int argc, char *argv[]) {
             case 's':
                 font_size = atoi(optarg);
                 break;
+            case 'F':
+                alt_font_family = optarg;
+                break;
+            case 'S':
+                alt_font_size = atoi(optarg);
+                break;
         }
     }
 
     int num = argc - optind;
     char **args = argv + optind;
 
-    for (int i = 0; i < num; i++)
-        printf("%i%s", text_width(font_family, font_size, args[i]), i < (num - 1) ? " " : "\n");
+    if (alt_font_family == NULL) {
+        for (int i = 0; i < num; i++)
+            printf("%i%s", text_width(font_family, font_size, args[i]), i < (num - 1) ? " " : "\n");
+    } else {
+        char chunk[MAXLEN] = {0};
+        for (int i = 0; i < num; i++) {
+            char *s = args[i];
+            unsigned int j = 0, pos = 0, w = 0;
+            bool opa = false;
+            while (j < strlen(s) && pos < sizeof(chunk)) {
+                if (s[j] == ALT_MAGIC_CHAR) {
+                    if (s[j + 1] == ALT_BEGIN_SEQ && !opa) {
+                        chunk[pos] = '\0';
+                        w += text_width(font_family, font_size, chunk);
+                        opa = true;
+                        pos = 0;
+                        j += 2;
+                    } else if (s[j + 1] == ALT_END_SEQ && opa) {
+                        chunk[pos] = '\0';
+                        w += text_width(alt_font_family, alt_font_size, chunk);
+                        opa = false;
+                        pos = 0;
+                        j += 2;
+                    } else if (s[j + 1] == ALT_MAGIC_CHAR) {
+                        chunk[pos++] = ALT_MAGIC_CHAR;
+                        j += 2;
+                    } else {
+                        chunk[pos++] = s[j++];
+                    }
+                } else {
+                    chunk[pos++] = s[j++];
+                }
+            }
+            chunk[pos] = '\0';
+            if (opa)
+                w += text_width(alt_font_family, alt_font_size, chunk);
+            else
+                w += text_width(font_family, font_size, chunk);
+            printf("%i%s", w, i < (num - 1) ? " " : "\n");
+        }
+    }
 
     return EXIT_SUCCESS;
 }
--- textwidth.h
+++ textwidth.h
@@ -1,6 +1,9 @@
-#define FONT_FAMILY  "sans-serif"
-#define FONT_SIZE    11
-#define MAXLEN       256
-#define MIN(A, B)    ((A) < (B) ? (A) : (B))
+#define FONT_FAMILY     "sans-serif"
+#define FONT_SIZE       16
+#define ALT_MAGIC_CHAR  '\\'
+#define ALT_BEGIN_SEQ   '('
+#define ALT_END_SEQ     ')'
+#define MAXLEN          256
+#define MIN(A, B)       ((A) < (B) ? (A) : (B))
 
 int text_width(char *, int, char *);
